928b3c8c8d459c04507cefd26f34d109f4ea7f9d
Lorelei <loreleigab@hotmail.com>
2022-09-04 13:47:17 -0300

    Correção na listagem de local da dor

--- views/modulos/administrativo/local_dor_lista.php
+++ views/modulos/administrativo/local_dor_lista.php
@@ -1,19 +1,19 @@
 <?php
 
-use TrainingCenter\Controllers\Administrativo\MembroController;
-$membroObj = new MembroController();
+use TrainingCenter\Controllers\Administrativo\LocalDorController;
+$dorObj = new LocalDorController();
 
-$membros = $membroObj->listarMembro();
+$dores = $dorObj->listarLocalDor();
 ?>
 <!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-10">
-                <h1 class="m-0 text-dark">Membros</h1>
+                <h1 class="m-0 text-dark">Local da dor</h1>
             </div><!-- /.col -->
             <div class="col-2">
-                <a href="<?= SERVERURL ?>administrativo/membro_cadastro" class="btn btn-success btn-block"><i class="fas fa-plus"></i> Adicionar</a>
+                <a href="<?= SERVERURL ?>administrativo/local_dor_cadastro" class="btn btn-success btn-block"><i class="fas fa-plus"></i> Adicionar</a>
             </div><!-- /.col -->
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
@@ -35,19 +35,19 @@ $membros = $membroObj->listarMembro();
                         <table id="tabela" class="table table-bordered table-striped">
                             <thead>
                             <tr>
-                                <th>Membro dominante</th>
+                                <th>Local da dor</th>
                                 <th>Ação</th>
                             </tr>
                             </thead>
                             <tbody>
-                            <?php foreach ($membros as $membro): ?>
+                            <?php foreach ($dores as $dor): ?>
                             <tr>
-                                <td><?=$membro->membro_dominante ?></td>
+                                <td><?=$dor->local_da_dor ?></td>
                                 <td class="d-flex flex-row justify-content-around">
-                                    <a href="<?= SERVERURL . "administrativo/membro_cadastro&id=" . $membroObj->encryption($membro->id) ?>" class="btn btn-sm btn-primary"><i class="fas fa-edit"></i> Editar</a>
-                                    <form class="form-horizontal formulario-ajax" method="POST" action="<?= SERVERURL ?>ajax/membroAjax.php" role="form" data-form="delete">
-                                        <input type="hidden" name="_method" value="apagaMembro">
-                                        <input type="hidden" name="id" value="<?= $membro->id ?>">
+                                    <a href="<?= SERVERURL . "administrativo/local_dor_cadastro&id=" . $dorObj->encryption($dor->id) ?>" class="btn btn-sm btn-primary"><i class="fas fa-edit"></i> Editar</a>
+                                    <form class="form-horizontal formulario-ajax" method="POST" action="<?= SERVERURL ?>ajax/localDorAjax.php" role="form" data-form="delete">
+                                        <input type="hidden" name="_method" value="apagaLocalDor">
+                                        <input type="hidden" name="id" value="<?= $dor->id ?>">
                                         <button type="submit" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i> Apagar</button>
                                         <div class="resposta-ajax"></div>
                                     </form>
@@ -57,7 +57,7 @@ $membros = $membroObj->listarMembro();
                             </tbody>
                             <tfoot>
                             <tr>
-                                <th>Membro dominante</th>
+                                <th>LocalDor dominante</th>
                                 <th>Ação</th>
                             </tr>
                             </tfoot>
