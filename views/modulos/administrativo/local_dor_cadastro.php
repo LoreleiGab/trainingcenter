1f45285f9b87feca14f1171eb0967533678f63a9
Lorelei <loreleigab@hotmail.com>
2022-09-04 13:47:41 -0300

    Correção no cadastro do local da dor

---
+++ views/modulos/administrativo/local_dor_cadastro.php
@@ -0,0 +1,64 @@
+<?php
+
+use TrainingCenter\Controllers\Administrativo\LocalDorController;
+$localDorObj = new LocalDorController();
+
+$id = $_GET['id'] ?? null;
+
+if ($id){
+    $localDor = $localDorObj->recuperarLocalDor($id);
+}
+?>
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Cadastro de local da dor</h1>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Horizontal Form -->
+                <div class="card card-info">
+                    <div class="card-header">
+                        <h3 class="card-title">Dados</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <form class="form-horizontal formulario-ajax" method="POST" action="<?= SERVERURL ?>ajax/localDorAjax.php" role="form" data-form="<?= ($id) ? "update" : "save" ?>">
+                    <input type="hidden" name="_method" value="<?= ($id) ? "editaLocalDor" : "cadastraLocalDor" ?>">
+                    <?php if ($id): ?>
+                        <input type="hidden" name="id" value="<?= $id ?>">
+                    <?php endif; ?>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="form-group col-md">
+                                    <label for="local_da_dor">Local da dor: *</label>
+                                    <input type="text" class="form-control" id="local_da_dor" name="local_da_dor" maxlength="1" value="<?= $localDor->local_da_dor ?? null ?>" required>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            <a href="<?= SERVERURL ?>administrativo/local_dor_lista">
+                                <button type="button" class="btn btn-default pull-left">Voltar</button>
+                            </a>
+                            <button type="submit" class="btn btn-info float-right">Gravar</button>
+                        </div>
+                        <!-- /.card-footer -->
+                        <div class="resposta-ajax"></div>
+                    </form>
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
